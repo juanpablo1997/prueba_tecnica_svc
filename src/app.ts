9377c82b99147acb374c69caf291dc1ec919e6f3
juanpablo1997 <juanpabloorozco15@gmail.com>
2024-10-31 18:02:13 -0500

    Refactor: Se realizan ajsutes para que la url sea publica para efectos de prueba

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,30 @@
-import express, { ErrorRequestHandler } from "express"
-import expenseController from "./routes/expenseController"
-import { initSequelize } from "./models/index"
+import express, { ErrorRequestHandler } from "express";
+import expenseController from "./routes/expenseController";
+import { initSequelize } from "./models/index";
+import cors from 'cors'; // Importa cors aquí
 
-initSequelize()
+// Inicializa la conexión con Sequelize
+initSequelize();
 
-const app = express()
-app.use(express.json())
+const app = express();
+
+// Configura CORS para permitir solicitudes desde cualquier origen
+app.use(cors()); // Corrige aquí
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Rutas
 app.use("/expenses", expenseController);
-const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  const responseStatus = err.response?.status || err.status || 500
 
+// Manejo de errores
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  const responseStatus = err.response?.status || err.status || 500;
   res.status(responseStatus).send({
     message: err.message,
-  })
-}
-app.use(errorHandler)
+  });
+};
+
+app.use(errorHandler);
 
 export default app;
